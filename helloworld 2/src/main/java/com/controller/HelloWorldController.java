dae903754db63baa9af9b8b3c9b32b23b9e5aa3b
Rebecca-Su <41730355+Rebecca-Su@users.noreply.github.com>
2018-07-27 10:44:31 +0800

    Update HelloWorldController.java

--- helloworld 2/src/main/java/com/controller/HelloWorldController.java
+++ helloworld 2/src/main/java/com/controller/HelloWorldController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldController{
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String sayHello(Model model) {
-		return "hello"; 
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public ModelAndView getDetail(@PathVariable(value = "name") String name){
+		ModelAndView m = new ModelAndView();
+		m.addObject("name", name);
+		m.setViewName("hello-name");
+		return m;
 	}
+	
 	
 }
