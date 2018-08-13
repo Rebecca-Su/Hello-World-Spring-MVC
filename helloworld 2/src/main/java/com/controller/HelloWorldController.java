c57178bdc58ffe77aecf732f2291e03cf0fcd3d7
Rebecca-Su <41730355+Rebecca-Su@users.noreply.github.com>
2018-08-13 15:06:09 +0800

    Update HelloWorldController.java

--- helloworld 2/src/main/java/com/controller/HelloWorldController.java
+++ helloworld 2/src/main/java/com/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.dao.UserMapper;
+import com.service.UserService;
 import com.entity.User;
 
 
