41c9662f60085a463645afdaa185339af38ad265
Rebecca-Su <41730355+Rebecca-Su@users.noreply.github.com>
2018-08-13 15:05:05 +0800

    Update HelloWorldController.java

--- helloworld 2/src/main/java/com/controller/HelloWorldController.java
+++ helloworld 2/src/main/java/com/controller/HelloWorldController.java
@@ -1,37 +1,33 @@
 package com.controller;
 
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.dao.UserMapper;
 import com.entity.User;
-
+import com.service.UserService;
 
 @Controller
-@RequestMapping(value = "/hello")
-public class HelloWorldController{
+public class HelloWorldController {
+	
 	@Resource
-	private UserMapper userMapper;
+	private UserService userService;
+	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String sayHello(Model model) {
-		return "hello"; 
+		return "welcome";
 	}
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public ModelAndView getDetail(@PathVariable(value="name")String name) {
-		ModelAndView m = new ModelAndView();
-        User a = userMapper.selectByE(name);
-		m.addObject("name", a);  
-		m.setViewName("hello-name");
-		return m;
 
+	@ResponseBody
+	@RequestMapping(value = "/getName", method = RequestMethod.GET)
+	public User getJsonDetail(String name) {
+		 User user = userService.selectByE(name);
+		return user;
 	}
 	
+	
 }
+	
